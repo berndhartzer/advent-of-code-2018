fce22bddef6c37d281b0c55e7b8d724bf8803e84
Bernd Hartzer <berndhartzer@gmail.com>
2018-12-06 20:35:03 +1000

    day 3 part two

--- 3-no-matter-how-you-slice-it.js
+++ 3-no-matter-how-you-slice-it.js
@@ -1,3 +1,7 @@
+/**
+ * Use at your own risk - these functions are extremely inefficient...
+ */
+
 const noMatterHowYouSliceItPartOne = (input) => {
   const fabric = input.reduce((grid, row) => {
     const [claim, x, y, w, h] = row.match(/\d+/g).map(n => Number(n));
@@ -22,6 +26,37 @@ const noMatterHowYouSliceItPartOne = (input) => {
   return Object.values(fabric).filter(u => u > 1).length;
 };
 
+const noMatterHowYouSliceItPartTwo = (input) => {
+  const fabric = input.reduce((grid, row) => {
+    const [claim, x, y, w, h] = row.match(/\d+/g).map(n => Number(n));
+    const width = [...Array(w).keys()];
+    const height = [...Array(h).keys()];
+
+    return {
+      ...grid,
+      ...width.reduce((outer, ww) => {
+        return {
+          ...outer,
+          ...height.reduce((inner, hh) => {
+            const coord = `${x + ww},${y + hh}`;
+            const claimHere = typeof grid[coord] !== 'undefined' ? [...grid[coord], claim] : [claim];
+            return { ...inner, [coord]: claimHere };
+          }, {})
+        }
+      }, {})
+    }
+  }, {});
+
+  const knownOverlaps = [...new Set(Object.values(fabric).filter(o => o.length > 1).reduce((a, p) => a.concat(p)))];
+
+  return input.reduce((a, val) => {
+    const [claim, x, y, w, h] = val.match(/\d+/g).map(n => Number(n));
+    if (knownOverlaps.includes(claim)) return a;
+    return [...a, claim];
+  }, [])[0];
+};
+
 module.exports = {
   noMatterHowYouSliceItPartOne,
+  noMatterHowYouSliceItPartTwo,
 };
--- tests/3-no-matter-how-you-slice-it.test.js
+++ tests/3-no-matter-how-you-slice-it.test.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const {
   noMatterHowYouSliceItPartOne,
+  noMatterHowYouSliceItPartTwo,
 } = require('../3-no-matter-how-you-slice-it');
 
 describe('3. No Matter How You Slice It', () => {
@@ -22,6 +23,27 @@ describe('3. No Matter How You Slice It', () => {
         .filter(x => x);
       const output = noMatterHowYouSliceItPartOne(input);
       console.log(`Part one answer: ${output}`);
+    });
+  });
+
+  describe('Part two', () => {
+    test('Test one', () => {
+      const input = [
+        '#1 @ 1,3: 4x4',
+        '#2 @ 3,1: 4x4',
+        '#3 @ 5,5: 2x2',
+      ];
+      const output = noMatterHowYouSliceItPartTwo(input);
+      expect(output).toBe(3);
+    });
+
+    test('Actual input', () => {
+      const input = fs
+        .readFileSync('./inputs/3-no-matter-how-you-slice-it.txt', 'utf8')
+        .split('\n')
+        .filter(x => x);
+      const output = noMatterHowYouSliceItPartTwo(input);
+      console.log(`Part two answer: ${output}`);
     });
   });
 });
