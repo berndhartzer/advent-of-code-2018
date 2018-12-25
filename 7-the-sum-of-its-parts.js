06585e927f3390fe23c65e9fd45f357a449ab4db
Bernd Hartzer <berndhartzer@gmail.com>
2018-12-26 09:22:38 +1000

    day 7 part two

--- 7-the-sum-of-its-parts.js
+++ 7-the-sum-of-its-parts.js
@@ -31,8 +31,69 @@ const theSumOfItsPartsPartOne = (input, steps = null, completed = []) => {
   return completed.join('');
 };
 
-const theSumOfItsPartsPartTwo = (input) => {
-  return 0;
+const theSumOfItsPartsPartTwo = (input, workers, stepDuration, steps = null, completed = [], work = [], counter = 0) => {
+  if (!steps) {
+    steps = input.reduce((a, instruction) => {
+      const parts = instruction.split(' ');
+      const step = parts[7];
+      const dep = parts[1];
+      const stepDeps = a[step] || [];
+
+      return {
+        ...a,
+        [step]: [...stepDeps, dep],
+        [dep]: a[dep] || [],
+      }
+    }, {});
+  }
+
+  const available = Object.entries(steps).reduce((a, [step, dep]) => {
+    if (!completed.includes(step)) {
+      if (dep.length === 0) return [...a, step]
+
+      const remainingDeps = dep.filter(x => !completed.includes(x));
+      if (remainingDeps.length === 0) return [...a, step];
+    }
+    return a;
+  }, []);
+
+  let availableWorkers = workers - work.length;
+
+  const addToWork = available.map(x => {
+    if (availableWorkers <= 0) return null;
+    if (work.find(y => y.step === x)) return null;
+
+    availableWorkers = availableWorkers - 1;
+    return {
+      step: x,
+      timeLeft: (x.charCodeAt(0) - 64) + stepDuration,
+    };
+  }).filter(z => z !== null);
+
+  const workUpdated = [...work, ...addToWork];
+
+  const workDone = workUpdated.map(x => {
+    return {
+      step: x.step,
+      timeLeft: x.timeLeft - 1,
+    };
+  });
+
+  const updatedCompleted = workDone.reduce((a, step) => {
+    if (step.timeLeft <= 0) return [...a, step.step];
+    return a;
+  }, completed);
+
+  const workFinished = workDone.filter(step => {
+    return !step.timeLeft <= 0;
+  });
+
+  const newCount = counter + 1;
+
+  if (Object.keys(steps).length !== updatedCompleted.length) {
+    return theSumOfItsPartsPartTwo(input, workers, stepDuration, steps, updatedCompleted, workFinished, newCount);
+  }
+  return newCount;
 };
 
 module.exports = {
--- tests/7-the-sum-of-its-parts.test.js
+++ tests/7-the-sum-of-its-parts.test.js
@@ -30,12 +30,19 @@ describe('7. The Sum of Its Parts', () => {
     });
   });
 
-  describe('Part two', () => {
+  describe.only('Part two', () => {
     test('Test one', () => {
       const input = [
+        'Step C must be finished before step A can begin.',
+        'Step C must be finished before step F can begin.',
+        'Step A must be finished before step B can begin.',
+        'Step A must be finished before step D can begin.',
+        'Step B must be finished before step E can begin.',
+        'Step D must be finished before step E can begin.',
+        'Step F must be finished before step E can begin.',
       ];
-      const output = theSumOfItsPartsPartTwo(input, 32);
-      // expect(output).toBe();
+      const output = theSumOfItsPartsPartTwo(input, 2, 0);
+      expect(output).toBe(15);
     });
 
     test('Actual input', () => {
@@ -43,7 +50,7 @@ describe('7. The Sum of Its Parts', () => {
         .readFileSync('./inputs/7-the-sum-of-its-parts.txt', 'utf8')
         .split('\n')
         .filter(x => x);
-      const output = theSumOfItsPartsPartTwo(input);
+      const output = theSumOfItsPartsPartTwo(input, 5, 60);
       console.log(`Part two answer: ${output}`);
     });
   });
